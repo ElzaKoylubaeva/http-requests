ce0397fbc2929426ee033f7a15b29e147b9c0854
Elza Koylubaeva <zakirova_elza@mail.ru>
2022-10-03 19:49:01 +0500

    Initial commit

---
+++ src/main/java/ru/koylubaevaem/netology/Data.java
@@ -0,0 +1,55 @@
+package ru.koylubaevaem.netology;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Data {
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final String upvotes;
+
+    public Data(@JsonProperty("id") String id,
+                @JsonProperty("text") String text,
+                @JsonProperty("type") String type,
+                @JsonProperty("user") String user,
+                @JsonProperty("upvotes") String upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes='" + upvotes + '\'' +
+                '}';
+    }
+}
